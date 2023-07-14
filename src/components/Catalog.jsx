f3165fc979e8b887a6a89a3e32c3bf6914bb2dbb
finikQ <86006560+finikQ@users.noreply.github.com>
2023-07-14 17:48:13 +0300

    initial commit

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -4,12 +4,13 @@ import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import styles from "./catalog.module.css";
 
 export const Catalog = (props) => {
   return (
-    <>
+    <div className={styles.productList}>
       {props.props.map((post) => (
-        <div key={post.id}>
+        <div key={post.id} className={styles.product}>
           <a href={`/clother/${post.id}`}>
             <Image
               src={post.images[0]}
@@ -24,7 +25,7 @@ export const Catalog = (props) => {
           </div>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -1,13 +1,60 @@
+import Image from "next/image";
 import Link from "next/link";
+import styles from "./theheader.module.css";
 
 const TheHeader = () => {
   return (
-    <header>
-      <Link href="/new">Новинки</Link>
-      <Link href="/clother">Одежда</Link>
-      <Link href="/accessories">Аксессуары</Link>
-      <Link href="/brands">Бренды</Link>
-      <Link href="/media">Медиа</Link>
+    <header className={styles.header}>
+      <div>
+        <a>
+          <Image
+            src="http://localhost:3000/vercel.svg"
+            alt="asd"
+            width={130}
+            height={22}
+          />
+        </a>
+      </div>
+      <nav className={styles.menu}>
+        <Link href="/new">Новинки</Link>
+        <Link href="/clother">Одежда</Link>
+        <Link href="/accessories">Аксессуары</Link>
+        <Link href="/brands">Бренды</Link>
+        <Link href="/media">Медиа</Link>
+      </nav>
+      <div className={styles.search}>
+        <form>
+          <input placeholder="Найти"></input>
+        </form>
+      </div>
+      <div className={styles.toolbar}>
+        <div>
+          <Image
+            src="http://localhost:3000/Profile.svg"
+            alt="favorites"
+            width={20}
+            height={20}
+          />
+        </div>
+        <a>
+          <Image
+            src="http://localhost:3000/heart.svg"
+            alt="favorites"
+            width={20}
+            height={20}
+          />
+          2
+        </a>
+        <a>
+          <Image
+            src="http://localhost:3000/Cart.svg"
+            alt="cart"
+            width={20}
+            height={20}
+          />
+          <div>4</div>
+        </a>
+      </div>
     </header>
   );
 };
