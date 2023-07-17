4e647aaaaa77b86a7f66fe33bb011a571b81b45f
finikQ <86006560+finikQ@users.noreply.github.com>
2023-07-17 17:32:17 +0300

    feat(footer css)

--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -1,34 +1,81 @@
+import Link from "next/link";
+import styles from "./thefooter.module.css";
+import Image from "next/image";
+
 const TheFooter = () => {
+  const socials = [
+    {
+      name: "telegram",
+      href: "https://t.me/sda_store",
+      img_src: "social/telegram.svg",
+    },
+    {
+      name: "instagram",
+      href: "https://www.instagram.com/sda_store/",
+      img_src: "social/instagram.svg",
+    },
+    {
+      name: "tiktok",
+      href: "https://www.tiktok.com/@sda_store",
+      img_src: "social/tiktok.svg",
+    },
+    {
+      name: "pinterest",
+      href: "https://www.pinterest.com/sda_store",
+      img_src: "social/pinterest.svg",
+    },
+  ];
+
   return (
-    <footer>
+    <footer className={styles.footer}>
       <div>
-        <h3>Помощь</h3>
-        <p>Доставка и Оплата</p>
-        <p>Вопросы и Ответы</p>
-        <p>О Компании</p>
-        <p>Медиа</p>
+        <h3>Каталог</h3>
+        <Link href="/new">Новинки</Link>
+        <Link href="/clother">Одежда</Link>
+        <Link href="/accessories">Аксессуары</Link>
+        <Link href="/brands">Бренды</Link>
+        <Link href="/media">Медиа</Link>
       </div>
       <div>
-        <h3>Контакты</h3>
-        <p>тел.: 123-456-789</p>
-        <p>почта: example@example.com</p>
-        <p>адрес: ул. Примерная, 123</p>
+        <h3>Помощь</h3>
+        <Link href="/common/delivery">Доставка</Link>
+        <Link href="/common/payment">Оплата</Link>
+        <Link href="/common/size">Выбор размера</Link>
+        <Link href="/media">Медиа</Link>
       </div>
       <div>
         <h3>Бренды</h3>
-        <p>Бренд 1</p>
-        <p>Бренд 2</p>
-        <p>Бренд 3</p>
-        <p>Бренд 4</p>
-        <p>Бренд 5</p>
+        <Link href="/common/brand1">Бренд 1</Link>
+        <Link href="/common/brand2">Бренд 2</Link>
+        <Link href="/common/brand3">Бренд 3</Link>
+        <Link href="/common/brand4">Бренд 4</Link>
+        <Link href="/common/brand5">Бренд 5</Link>
       </div>
       <div>
-        <h3>Категории</h3>
-        <p>Категория 1</p>
-        <p>Категория 2</p>
-        <p>Категория 3</p>
-        <p>Категория 4</p>
-        <p>Категория 5</p>
+        <h3>Контакты</h3>
+        <div>
+          <Link href="tel:8-123-456-78-90">8-123-456-78-90</Link>
+        </div>
+        <div>
+          <Link href="mailto:example@example.com">example@example.com</Link>
+        </div>
+        <div>ул. Примерная, 123</div>
+        <div>
+          <ul className={styles["social-links"]}>
+            {socials.map((item): any => (
+              <Link key={item.name} href={item.href} target="_blank">
+                <li>
+                  <Image
+                    src={item.img_src}
+                    alt={item.name}
+                    width={40}
+                    height={40}
+                  />
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
       </div>
     </footer>
   );
