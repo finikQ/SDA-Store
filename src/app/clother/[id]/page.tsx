59db8d5f005330e881b741d0b2ec1e0028ad0b85
finikQ <86006560+finikQ@users.noreply.github.com>
2023-07-18 09:21:38 +0300

    style(product page)

--- src/app/clother/[id]/page.tsx
+++ src/app/clother/[id]/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import styles from "./page.module.css";
 
 type Props = {
   params: {
@@ -15,28 +16,55 @@ export default async function Clother({ params: { id } }: Props) {
   const product = await getProduct(id);
 
   return (
-    <>
-      <div>
-        <Image
-          src={product.images[0]}
-          alt={product.description}
-          width={250}
-          height={250}
-        ></Image>
-      </div>
-      <div>
-        <h1>{product.title}</h1>
+    <div>
+      <div>Breadcrumb</div>
+
+      <div className={styles.container}>
         <div>
-          <div>{product.price} $</div>
-          <div>{product.rating} *</div>
+          <Image
+            src={product.images[0]}
+            alt={product.description}
+            width={550}
+            height={550}
+          />
+          <div>
+            {product.images.map((image) => (
+              <Image
+                key={image}
+                src={image}
+                alt="image"
+                width={100}
+                height={100}
+              />
+            ))}
+          </div>
         </div>
-        <div>
-          <button>
-            <span>Добавить в корзину</span>
-          </button>
-          <span>{"<3"}</span>
+
+        <div className={styles.info}>
+          <h1>{product.title}</h1>
+          <div className={styles.price}>
+            <div>{product.price} $</div>
+            <s>
+              {Math.round(
+                product.price / (1 - product.discountPercentage / 100)
+              )}{" "}
+              $
+            </s>
+          </div>
+
+          <div className={styles.buttons}>
+            <button className={styles.toCart}>В Корзину</button>
+            <button>
+              <Image src="/heart.svg" alt="favorites" width={20} height={20} />
+            </button>
+          </div>
+
+          <div className={styles.description}>
+            <div>Бренд: {product.brand}</div>
+            <div>Описание: {product.description}</div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
