d1a15862f63d686d132b4caa1a5486ce7bc722fc
finikQ <86006560+finikQ@users.noreply.github.com>
2023-07-26 03:00:16 +0300

    feat(filters) Функционал фильтрам в каталоге

--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -1,26 +1,103 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./catalog.module.css";
 import { Product } from "./product/Product";
 import { typeCartItem } from "@/redux/features/cart-slice";
 import { Filters } from "./filters/Filters";
 
 export const Catalog: React.FC<{ props: typeCartItem[] }> = ({ props }) => {
+  const [filters, setFilters] = useState<{
+    brands: string[];
+    minPrice: number;
+    maxPrice: number;
+    sizes: string[];
+  }>({
+    brands: [],
+    minPrice: 0,
+    maxPrice: 100000,
+    sizes: [],
+  });
+
+  let filteredProps: typeCartItem[] = props.filter((product) => {
+    const brandFilter =
+      filters.brands.length === 0 || filters.brands.includes(product.brand);
+    const priceFilter =
+      product.price >= filters.minPrice && product.price <= filters.maxPrice;
+    const sizeFilter =
+      filters.sizes.length === 0 || filters.sizes.includes(product.category);
+    return brandFilter && priceFilter && sizeFilter;
+  });
+
+  useEffect(() => {
+    let priceSet: Array<number> = filteredProps.map((item) => item.price);
+    setFilters((prevState: any) => ({
+      ...prevState,
+      minPrice: Math.min.apply(null, priceSet),
+      maxPrice: Math.max.apply(null, priceSet),
+    }));
+  }, []);
+  let priceSet: Array<number> = props.map((item) => item.price);
+
+  const clearBrandFilter = () => {
+    setFilters({ ...filters, brands: [] });
+  };
+
+  const clearPriceFilter = () => {
+    setFilters({
+      ...filters,
+      minPrice: Math.min.apply(null, priceSet),
+      maxPrice: Math.max.apply(null, priceSet),
+    });
+  };
+
+  const clearSizeFilter = () => {
+    setFilters({ ...filters, sizes: [] });
+  };
+
+  console.log(
+    Math.min.apply(null, priceSet),
+    " ",
+    Math.max.apply(null, priceSet)
+  );
+
   return (
     <div>
       <div className={styles.breadcrumb}>
         <div>Breadcrumb</div>
-        <div>Active Filters</div>
+        <div>
+          {filters.brands.length > 0 ? (
+            <div>
+              <button onClick={clearBrandFilter}>
+                <span>Бренд</span>
+              </button>
+            </div>
+          ) : null}
+          {filters.minPrice != Math.min.apply(null, priceSet) ||
+          filters.maxPrice != Math.max.apply(null, priceSet) ? (
+            <div>
+              <button onClick={clearPriceFilter}>
+                <span>Цена</span>
+              </button>
+            </div>
+          ) : null}
+          {filters.sizes.length > 0 ? (
+            <div>
+              <button onClick={clearSizeFilter}>
+                <span>Размер</span>
+              </button>
+            </div>
+          ) : null}
+        </div>
       </div>
       <div className={styles.toolbar}>Some buttons</div>
       <div className={styles.mainWrapper}>
         <div className={styles.filters}>
-          <Filters props={props} />
+          <Filters props={props} filters={filters} setFilters={setFilters} />
         </div>
         <div className={styles.productList}>
-          {props.map((product) => (
+          {filteredProps.map((product) => (
             <Product key={product.id} product={product} />
           ))}
         </div>
--- src/components/catalog/filters/Filters.tsx
+++ src/components/catalog/filters/Filters.tsx
@@ -1,22 +1,46 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./filters.module.css";
 import { typeCartItem } from "@/redux/features/cart-slice";
 
-export const Filters: React.FC<{ props: typeCartItem[] }> = ({ props }) => {
-    const [visibleFilter, setVisibleFilters] = useState ({
+export const Filters: React.FC<{
+  props: typeCartItem[];
+  filters: any;
+  setFilters: any;
+}> = ({ props, filters, setFilters }) => {
+  const [visibleFilter, setVisibleFilters] = useState({
     visibleBrands: 5,
-    visiblePrices: 5,
     visibleSizes: 5,
   });
 
   const handleShowAllBrands = () => {
-    setVisibleFilters({ ...visibleFilter, visibleBrands: brandSet.size });
+    if (visibleFilter.visibleBrands !== 5) {
+      setVisibleFilters({ ...visibleFilter, visibleBrands: 5 });
+    } else {
+      setVisibleFilters({ ...visibleFilter, visibleBrands: brandSet.size });
+    }
+  };
+
+  const handleMinPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilters({ ...filters, minPrice: Number(event.target.value) });
+  };
+
+  const handleMaxPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilters({ ...filters, maxPrice: Number(event.target.value) });
+  };
+
+  const handleShowAllSizes = () => {
+    if (visibleFilter.visibleSizes !== 5) {
+      setVisibleFilters({ ...visibleFilter, visibleSizes: 5 });
+    } else {
+      setVisibleFilters({ ...visibleFilter, visibleSizes: sizeSet.size + 1 });
+    }
   };
 
   let brandSet: Set<string> = new Set(props.map((item) => item.brand));
+  let sizeSet: Set<string> = new Set(props.map((item) => item.category));
   return (
     <div>
       <aside>
@@ -24,18 +48,101 @@ export const Filters: React.FC<{ props: typeCartItem[] }> = ({ props }) => {
           <div>Бренд</div>
           <div>
             <ul>
-              {Array.from(brandSet).map((brand, index) => (
-            index < visibleFilter.visibleBrands && <li key={brand}>{brand}</li>
-          ))}
+              {Array.from(brandSet).map(
+                (mapItem, index) =>
+                  index < visibleFilter.visibleBrands && (
+                    <li key={mapItem}>
+                      <label>
+                        <input
+                          type="checkbox"
+                          checked={filters.brands.includes(mapItem)}
+                          onChange={() => {
+                            if (filters.brands.includes(mapItem)) {
+                              setFilters({
+                                ...filters,
+                                brands: filters.brands.filter(
+                                  (brand: any) => brand !== mapItem
+                                ),
+                              });
+                            } else {
+                              setFilters({
+                                ...filters,
+                                brands: [...filters.brands, mapItem],
+                              });
+                            }
+                          }}
+                        />
+                        {mapItem}
+                      </label>
+                    </li>
+                  )
+              )}
             </ul>
-            {visibleFilter.visibleBrands < brandSet.size && (
-          <span onClick={handleShowAllBrands}>Показать все</span>
-        )}
+            <span onClick={handleShowAllBrands}>
+              {visibleFilter.visibleBrands === 5 ? "Показать все" : "Скрыть"}
+            </span>
           </div>
         </div>
 
-        <div>Цена</div>
-        <div>Размер</div>
+        <div>
+          <div>Цена</div>
+          <div>
+            <input
+              type="number"
+              pattern="[0-9]*"
+              inputMode="numeric"
+              value={filters.minPrice}
+              onChange={handleMinPriceChange}
+            />
+            <input
+              type="number"
+              pattern="[0-9]*"
+              inputMode="numeric"
+              value={filters.maxPrice}
+              onChange={handleMaxPriceChange}
+            />
+          </div>
+        </div>
+
+        <div>
+          <div>Размер</div>
+          <div>
+            <ul>
+              {Array.from(sizeSet).map(
+                (mapItem, index) =>
+                  index < visibleFilter.visibleSizes && (
+                    <li key={mapItem}>
+                      <label>
+                        <input
+                          type="checkbox"
+                          checked={filters.sizes.includes(mapItem)}
+                          onChange={() => {
+                            if (filters.sizes.includes(mapItem)) {
+                              setFilters({
+                                ...filters,
+                                sizes: filters.sizes.filter(
+                                  (size: any) => size !== mapItem
+                                ),
+                              });
+                            } else {
+                              setFilters({
+                                ...filters,
+                                sizes: [...filters.sizes, mapItem],
+                              });
+                            }
+                          }}
+                        />
+                        {mapItem}
+                      </label>
+                    </li>
+                  )
+              )}
+            </ul>
+            <span onClick={handleShowAllSizes}>
+              {visibleFilter.visibleSizes === 5 ? "Показать все" : "Скрыть"}
+            </span>
+          </div>
+        </div>
       </aside>
     </div>
   );
